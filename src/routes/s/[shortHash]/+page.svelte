ecf3c0887615214cff73bcfa2782e5b093250694
nisu joshi <nisarg.joshi.95@gmail.com>
2025-02-15 04:54:09 -0500

    update download unlock button

--- src/routes/s/[shortHash]/+page.svelte
+++ src/routes/s/[shortHash]/+page.svelte
@@ -170,7 +170,21 @@
 						required
 					/>
 				</div>
-				<Button type="submit" color="primary" class="w-full">unlock</Button>
+				<Button type="submit" color="green" class="w-full">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="mr-2 h-4 w-4 text-gray-400"
+						viewBox="0 0 20 20"
+						fill="white"
+					>
+						<path
+							fill-rule="evenodd"
+							d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+							clip-rule="evenodd"
+						/>
+					</svg>
+					unlock
+				</Button>
 			</form>
 		{:else}
 			<div class="space-y-4">
